01fca3d39337ed687ef14015e55b0333e468b9ca
whatvlad <vladennopwnz@gmail.com>
2021-02-12 18:47:28 +0200

    text

---
+++ ls-30/script.js
@@ -0,0 +1,47 @@
+let a = prompt("Как вас зовут?")
+console.log(a)
+alert("Здравствуйте, " + a)
+
+let b = prompt("Сколько вам лет?")
+console.log(b)
+alert("Ваш возраст - " + b)
+
+let c = prompt("Где вы проживаете на данный момент?")
+console.log(c)
+alert("Ваше место жительства - " + c)
+
+let d = prompt("Какое образование вы имеете?")
+console.log(d)
+alert("Ваше образование - " + d)
+
+let e = prompt("Ваши преимущества?")
+console.log(e)
+alert("Вы - " + e)
+
+let f = prompt("Какие языки программрования вы изучали?")
+console.log(f)
+alert("Вы изучали - " + f)
+
+let g = prompt("Что вы разрабатывали раннее?")
+console.log(g)
+alert("Ваши разработки: " + g)
+
+let h = prompt("Кем вы видите себя в будущем?")
+console.log(h)
+alert("В будущем вы видите себя - " + h)
+
+let j = prompt("Ваши слабые стороны?")
+console.log(j)
+alert("Ваши слабые стороны: " + j)
+
+let k = prompt("Какой у вас стаж работы?")
+console.log(k)
+alert("Ваш стаж работы: " + k)
+
+let l = prompt("Что вы больше всего цените в коллективе?")
+console.log(l)
+alert("Вы цените - " + l)
+
+let n = prompt("Что вы надеетесь получать от этой работы?")
+console.log(n)
+alert("Вы надеетесь получать: " + n)
