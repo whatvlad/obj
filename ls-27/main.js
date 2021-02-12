67ab11f4abde1a227582afa54823ce336e29d8b1
Dana123098 <danaqwert5@gmail.com>
2021-02-12 16:34:28 +0200

    text

---
+++ ls-27/main.js
@@ -0,0 +1,42 @@
+// // let x = 15
+// // let y = 16
+// // let z = 17
+
+// let x = 13
+//     y = 126
+//     z = 136
+
+//     c = x + y + z
+//     v = x - y - z
+//     b = x * y * z
+//     n = x / y / z
+//     h = "fsafsaf"
+// // document.writeln(c)
+// console.log(c)
+// console.log(v)
+// console.log(b)
+// console.log(n)
+// document.writeln(h)
+
+
+// let x = prompt("Как тебя зовут?");
+// alert("Привет, " + x);
+
+
+let x = prompt("Как тебя зовут?");
+alert("О, тебя зовут " + x);
+console.log(x);
+
+let y = prompt("Сколько тебе лет?");
+alert("Ого, тебе уже " + y + " лет");
+console.log(y);
+
+let z = prompt("Кем ты работаешь?");
+alert("Классно, ты работаешь " + z);
+console.log(z);
+
+let c = prompt("Как твое настроение?");
+alert("Тебе " + c);
+console.log(c);
+
+
