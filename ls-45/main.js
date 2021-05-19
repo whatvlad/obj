78a8a62e37c15fabcc0fe03622390a6d7c306759
whatvlad <78967737+whatvlad@users.noreply.github.com>
2021-05-19 16:55:01 +0300

    Add files via upload

---
+++ ls-45/main.js
@@ -0,0 +1,10 @@
+let a = document.getElementById("text5");
+// b = a.innerHTML;
+// a.innerHTML = "Здесь был параграф 3";
+// b = b + "А";
+// alert(b);
+// a.innerHTML = "Саша, ты готов?";
+// console.log(a.innerHTML);
+// let l = document.getElementsByClassName("biba");
+// console.log(l);
+a.setAttribute("class", "n")
