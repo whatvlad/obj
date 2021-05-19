d7f09a8590e2ffd95c538ca6311c598a2f8e79e8
whatvlad <78967737+whatvlad@users.noreply.github.com>
2021-05-19 16:54:34 +0300

    Add files via upload

---
+++ ls-44/main.js
@@ -0,0 +1,45 @@
+// function showMessage(){
+// 	alert("Привет!")
+// }
+// showMessage();
+
+// function showMessage(){
+// 	let a = "Привет!"
+// 	alert(a)
+// }
+// showMessage()
+
+// function showMessage(){
+// 	let a = prompt("Как тебя зовут")
+// 	alert(a)
+// }
+// showMessage();
+
+// let myName = prompt("Как тебя зовут?")
+// function showMessage(){
+// 	let a = "Привет, " + " " + myName
+// 	alert(a)
+// }
+// showMessage();
+
+// function showMessage(from, text){
+// 	alert(from + " " + "получит" + " " + text)
+// }
+// showMessage("Vlad", "diplom")
+
+// function showMessage(a, b){
+// 	alert(a + " " + "Геркулес ел кашу" + " " + b)
+// }
+// let a = prompt("Как Геркулес ел кашу?")
+// 	 b = prompt("Где?")
+
+// showMessage(a, b);
+
+
+// let a = Number(prompt("a ="))
+// 	 b = Number(prompt("b ="))
+// function sum(a, b){
+// 	return a + b
+// }
+// let c = sum(a, b)
+// alert(c)
